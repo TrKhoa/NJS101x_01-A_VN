52bba4dfcab784c6fb2e3554acb5cead9a45893c
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-06-12 23:13:15 +0700

    fn

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url == '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head></head>');
+    res.write('<body>');
+    res.write("<form action='/message' method='POST'>");
+    res.write("<input type='text' name='message' /><button type='submit'>chuyen trang</button>");
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+  } else if (url === '/message') {
+    if (method === 'POST') {
+      const body=[];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      });
+      req.on('end', ()=> {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt', message);
+      });
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      res.end();
+    }
+  }
+})
+
+server.listen('3000')
