d0b5f7175f3b1a76d29d02d3c8c9e890dfcd517a
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-06-12 11:16:02 +0700

    fn

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head></head>');
+  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+  res.write('</html>');
 })
 
 server.listen('3000')
