b95c2f5524e52056c6ce3209be1669057a8b8288
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-06-12 11:16:52 +0700

    fn

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type','text/html');
-  res.write('<html>');
-  res.write('<head></head>');
-  res.write('<body><h1>Hello from my Node.js server!</h1></body>');
-  res.write('</html>');
+  const url = req.url;
+  if(url=='/'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head></head>');
+    res.write('<body>');
+    res.write("<input type='text' /><a href='/message'><input type='button' value='chuyen trang' /></a>");
+    res.write('</body>');
+    res.write('</html>');
+  } else if (url=='/message'){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('</html>');
+  }
 })
 
 server.listen('3000')
