bebcfe21b71baac403429c45ce97f5ee94fda2ae
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-06-19 09:31:38 +0700

    fn

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("dong 1");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("dong 2");
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
