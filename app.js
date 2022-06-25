284ac51b85819ef9e993a3c3032bdaf79a217f06
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-06-25 13:33:43 +0700

    fn

---
+++ app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const express = require('express');
+const bodyParse = require('body-parser');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/',(req,res,next)=>{
+  console.log('Đây là cái luôn chạy');
+  next();
+})
+
+app.use('/add-product',(req, res, next) => {
+    res.send("<h1>The Add product page</h1><form action='/product' method='POST'><input name='data'><button type='submit'>ok</button><form/>");
+});
+
+app.post('/product',(req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
+
+app.use((req, res, next) => {
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
