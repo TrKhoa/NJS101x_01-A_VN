c13b818ab8f397fa576b80b98936bdbf2092d9e2
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-07-05 11:57:07 +0700

    fn

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const path = require('path')
+
+const express = require('express');
+const bodyParse = require('body-parser');
+
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) =>{
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
+
+const server = http.createServer(app);
+
+server.listen(3000);
