d851fcb1d8853348e70658b2a50bacffd5851b11
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-07-05 14:01:09 +0700

    fn

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const rootDir = require('../util/path')
+
+const express = require('express');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log(adminData.products);
+  res.render('shop');
+});
+
+module.exports = router;
