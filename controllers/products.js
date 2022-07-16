5b765a9f8c88c71c763ee0163a40231939e34e48
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-07-16 14:02:17 +0700

    fn

---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+const Products = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+
+    res.render('add-product',{
+        pageTitle: 'Add product',
+        path: 'admin/add-product',
+        activeAddProduct: true,
+        productCSS: true,
+        formCSS: true
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const products = new Products(req.body.title);
+    products.save();
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    const products = Products.fetchAll();
+  res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProduct: products.length >0,
+      activeShop: true,
+      productCSS: true,
+  });
+};
+
+exports.products = Products;
