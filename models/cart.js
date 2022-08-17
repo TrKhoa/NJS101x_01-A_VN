d7ff6ddd618381b91156ebfe197e5296bbe330a6
TrKhoa <khoatmtfx14250@funix.edu.vn>
2022-08-18 00:01:52 +0700

    fn

---
+++ models/cart.js
@@ -0,0 +1,14 @@
+/*const Sq = require('sequelize');
+
+const sq = require('../util/database');
+
+const Cart = sq.define('cart', {
+    id: {
+        type: Sq.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Cart;*/
